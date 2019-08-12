9346e8b5dfb95d2505b7c5e7c8493c5d30c488fc
Sarovar <iamsarovarjs@gmail.com>
2019-08-12 21:45:03 +0530

    first-commit before changing eject

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { StyleRoot } from 'radium'; // required in case of applying media queries
+
+ReactDOM.render(
+  <StyleRoot>
+    <App />
+  </StyleRoot>,
+  document.getElementById('root')
+);
+registerServiceWorker();
